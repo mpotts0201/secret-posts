1e243098d1da57ef02c06de88add8afb4eb40c27
mpotts0201 <mpotts0201@gmail.com>
2018-05-03 18:10:05 -0400

    added ffaker and migrated posts model

--- Gemfile
+++ Gemfile
@@ -43,3 +43,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'omniauth'
 gem 'devise_token_auth'
+
+gem 'ffaker'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+
+  has_many :posts, dependent: :destroy
+
 end
